330bc7c2499bc9968edd827e30f31ab49f96aee3
rdgh0st <83750015+rdgh0st@users.noreply.github.com>
2024-08-16 21:32:32 -0700

    Day/Night Cycle Complete

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -15,29 +15,102 @@ public enum Daytime
 public class DayNightCycle : MonoBehaviour
 {
     private Vector3 rotation = Vector3.zero;
+    private Vector3 totalRotation = Vector3.zero;
     public Daytime daytime;
 
-    [SerializeField] private float DayTimeSeconds = 90f;
+    [SerializeField] private float DawnSeconds = 10f;
+    [SerializeField] private float DawnDegrees = 30f;
+
+    [SerializeField] private float DayTimeSeconds = 60f;
     [SerializeField] private float DayTimeDegrees = 150f;
 
-    [SerializeField]
-    private float degrees_per_second = 6;
+    [SerializeField] private float DuskSeconds = 20f;
+    [SerializeField] private float DuskDegrees = 30f;
 
-    // Update is called once per frame
+    [SerializeField] private float NightSeconds = 120f;
+    [SerializeField] private float NightDegrees = 150f;
     void Update()
     {
-        rotation.x = degrees_per_second * Time.deltaTime;
-        transform.Rotate(rotation, Space.World);
+        switch (daytime)
+        {
+            case Daytime.DAWN:
+                DawnUpdate();
+                break;
+            case Daytime.DAY:
+                DayUpdate();
+                break;
+            case Daytime.DUSK:
+                DuskUpdate();
+                break;
+            case Daytime.NIGHT:
+                NightUpdate();
+                break;
+        }
+        
+        CheckTime();
 
         // if daisy-chain
         // when set, do OnDayCycleChange?.Invoke(daytime);
     }
 
+    void CheckTime()
+    {
+        if (daytime != Daytime.DAWN && totalRotation.x < DawnDegrees)
+        {
+            daytime = Daytime.DAWN;
+            OnDayCycleChange?.Invoke(daytime);
+        } else if (daytime != Daytime.DAY && totalRotation.x >= DawnDegrees && totalRotation.x < DawnDegrees + DayTimeDegrees)
+        {
+            daytime = Daytime.DAY;
+            OnDayCycleChange?.Invoke(daytime);
+        } else if (daytime != Daytime.DUSK && totalRotation.x >= DawnDegrees + DayTimeDegrees && totalRotation.x < DawnDegrees + DayTimeDegrees + DuskDegrees)
+        {
+            daytime = Daytime.DUSK;
+            OnDayCycleChange?.Invoke(daytime);
+        } else if (daytime != Daytime.NIGHT && totalRotation.x >= DawnDegrees + DayTimeDegrees + DuskDegrees)
+        {
+            daytime = Daytime.NIGHT;
+            OnDayCycleChange?.Invoke(daytime);
+        }
+
+        if (totalRotation.x > 360)
+        {
+            totalRotation.x = 0;
+        }
+    }
+
+    void DawnUpdate()
+    {
+        float degrees_per_second = DawnDegrees / DawnSeconds;
+        rotation.x = degrees_per_second * Time.deltaTime;
+        transform.Rotate(rotation, Space.World);
+        totalRotation.x += rotation.x;
+    }
+
+    void DayUpdate()
+    {
+        float degrees_per_second = DayTimeDegrees / DayTimeSeconds;
+        rotation.x = degrees_per_second * Time.deltaTime;
+        transform.Rotate(rotation, Space.World);
+        totalRotation.x += rotation.x;
+    }
+
+    void DuskUpdate()
+    {
+        float degrees_per_second = DuskDegrees / DuskSeconds;
+        rotation.x = degrees_per_second * Time.deltaTime;
+        transform.Rotate(rotation, Space.World);
+        totalRotation.x += rotation.x;
+    }
+
+    void NightUpdate()
+    {
+        float degrees_per_second = NightDegrees / NightSeconds;
+        rotation.x = degrees_per_second * Time.deltaTime;
+        transform.Rotate(rotation, Space.World);
+        totalRotation.x += rotation.x;
+    }
+
     public delegate void DaytimeDelegate(Daytime daytime);
     public event DaytimeDelegate OnDayCycleChange;
-
-    public bool IsDayTime()
-    {
-        return rotation.x >= 0 && rotation.x < 180;
-    }
 }
