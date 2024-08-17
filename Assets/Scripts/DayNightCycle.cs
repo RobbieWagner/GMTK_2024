8ef2b9d763470e44a681d6c18cee45878e8a7ab2
Robbie_Wagner <robbie.zoborowi@gmail.com>
2024-08-16 22:18:34 -0500

    architecture

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Daytime
+{
+    NONE = 0,
+    DAWN = 1,
+    DAY = 2,
+    DUSK = 3,
+    NIGHT = 4
+}
+
 public class DayNightCycle : MonoBehaviour
 {
     private Vector3 rotation = Vector3.zero;
+    public Daytime daytime;
+
+    [SerializeField] private float DayTimeSeconds = 90f;
+    [SerializeField] private float DayTimeDegrees = 150f;
 
     [SerializeField]
     private float degrees_per_second = 6;
@@ -14,7 +28,13 @@ public class DayNightCycle : MonoBehaviour
     {
         rotation.x = degrees_per_second * Time.deltaTime;
         transform.Rotate(rotation, Space.World);
+
+        // if daisy-chain
+        // when set, do OnDayCycleChange?.Invoke(daytime);
     }
+
+    public delegate void DaytimeDelegate(Daytime daytime);
+    public event DaytimeDelegate OnDayCycleChange;
 
     public bool IsDayTime()
     {
